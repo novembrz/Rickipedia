ad2f272350888c634f811c608db444058c6404be
Dasha <51517187+novembrz@users.noreply.github.com>
2021-07-08 14:00:23 +0300

    pr fix

--- Rick&Morty/Rick&Morty/Services/NetworkService.swift
+++ Rick&Morty/Rick&Morty/Services/NetworkService.swift
@@ -11,9 +11,12 @@ import SwiftyJSON
 enum DecodeType {
     case all
     case random
+<<<<<<< HEAD
     case randomLocations
     case person
     case location
+=======
+>>>>>>> parent of fa54670 (Refactored the NetworkService)
 }
 
 struct NetworkService {
@@ -33,10 +36,9 @@ struct NetworkService {
             }
             //print("💡Network responce", response)
             
-            guard let json = try? JSON(data: data, options: JSONSerialization.ReadingOptions.mutableContainers) else {return}
-            
             switch decodeType {
             case .all:
+<<<<<<< HEAD
                 let persons = NetworkParser.appendPersons(from: json["results"].arrayValue, count: 20)
                 completion(persons as? T)
             case .random:
@@ -51,10 +53,58 @@ struct NetworkService {
             case .location:
                 let location = NetworkParser.appendOneLocation(from: json)
                 completion(location as? T)
+=======
+                let decoded = decodeAllJSON(type: T.self, from: data)
+                completion(decoded)
+            case .random:
+                let decoded = decodeRandomJSON(type: T.self, from: data)
+                completion(decoded)
+>>>>>>> parent of fa54670 (Refactored the NetworkService)
             }
         }
         task.resume()
     }
+<<<<<<< HEAD
+=======
+    
+    //MARK: Decode
+    
+    static func decodeAllJSON<T>(type: T.Type, from: Data?) -> T? {
+        guard let data = from else { return nil }
+        guard let json = try? JSON(data: data, options: JSONSerialization.ReadingOptions.mutableContainers) else {return nil}
+        let persons = personsAppend(from: json["results"].arrayValue, count: 20)
+        
+        return persons as? T
+    }
+    
+    static func decodeRandomJSON<T>(type: T.Type, from: Data?) -> T? {
+        guard let data = from else { return nil }
+        guard let json = try? JSON(data: data, options: JSONSerialization.ReadingOptions.mutableContainers) else {return nil}
+        let persons = personsAppend(from: json.arrayValue, count: 5)
+        
+        return persons as? T
+    }
+    
+    static func personsAppend(from json: [JSON], count: Int) -> [Person] {
+        var persons: [Person] = []
+        for i in 0..<count {
+            let name = json[i]["name"].stringValue
+            let id = json[i]["id"].intValue
+            let image = json[i]["image"].stringValue
+            let species = json[i]["species"].stringValue //раса
+            let gender = json[i]["gender"].stringValue
+            let status = json[i]["status"].stringValue
+            let type = json[i]["type"].stringValue
+            let url = json[i]["url"].stringValue
+            //let episode = arrayNames[i]["episode"].arrayValue
+            //let origin = arrayNames[i]["origin"].dictionaryValue
+            //let location = arrayNames[i]["location"].dictionaryValue
+            
+            persons.append(Person(index: i, id: id, name: name, image: image, status: status, species: species, type: type, gender: gender, url: url))
+        }
+        return persons
+    }
+>>>>>>> parent of fa54670 (Refactored the NetworkService)
 }
 
 
--- Rick&Morty/Rick&Morty/Views/Home/Carousel/CarouselContentView.swift
+++ Rick&Morty/Rick&Morty/Views/Home/Carousel/CarouselContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 import Kingfisher
 
+<<<<<<< HEAD
 //MARK: - ViewModel
 final class CarouselViewModel: ObservableObject {
     @Published var showCard = false
     @Published var id: Int?
     @Published var person: Person
     @Published var scrolled: Int
+=======
+struct CarouselContentView: View {
+    @EnvironmentObject var viewModel: CarouselViewModel
+    @State private var showCard = false
+    @State var name: String?
+>>>>>>> parent of fa54670 (Refactored the NetworkService)
     
     init(person: Person, scrolled: Int) {
         self.person = person
@@ -28,12 +35,21 @@ struct CarouselContentView: View {
     
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
+<<<<<<< HEAD
             KFImage(URL(string: viewModel.person.image ?? defaultImageUrl))
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     //динамический фрейм
                     .frame(width: SizeGenerator.calculateWidth(),
                            height: (SizeGenerator.height - CGFloat(viewModel.person.index - viewModel.scrolled) * 50))
+=======
+                KFImage(URL(string: person.image ?? "https://pristor.ru/wp-content/uploads/2018/11/%D0%A0%D0%B8%D0%BA-%D0%B8-%D0%9C%D0%BE%D1%80%D1%82%D0%B8-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%BD%D1%8B%D0%B5-%D0%B8-%D0%BA%D1%80%D1%83%D1%82%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-%D0%BD%D0%B0-%D0%B0%D0%B2%D1%83-%D0%B8-%D0%BD%D0%B0-%D0%B7%D0%B0%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D1%83-19-576x1024.jpg"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    //динамический фрейм
+                    .frame(width: calculateWidth(),
+                           height: (height - CGFloat(person.index - scrolled) * 50))
+>>>>>>> parent of fa54670 (Refactored the NetworkService)
                     .cornerRadius(15)
             
             VStack(alignment: .leading, spacing: 18) {
@@ -58,18 +74,26 @@ struct CarouselContentView: View {
                         .clipShape(Capsule())
                 }
             }
-            .frame(width: SizeGenerator.calculateWidth() - 40)
+            .frame(width: calculateWidth() - 40)
             .padding(.leading, 20)
             .padding(.bottom, 20)
         }
         .offset(x: viewModel.person.index - viewModel.scrolled <= 2 ? CGFloat(viewModel.person.index - viewModel.scrolled) * 30 : 60)
         .onTapGesture {
             withAnimation(.spring()) {
+<<<<<<< HEAD
                 viewModel.id = viewModel.person.id
                 viewModel.showCard.toggle()
             }
         }.sheet(isPresented: $viewModel.showCard) {
             PersonCardView(id: $viewModel.id)
+=======
+                name = person.name
+                showCard.toggle()
+            }
+        }.sheet(isPresented: $showCard) {
+            PersonCardView(name: $name)
+>>>>>>> parent of fa54670 (Refactored the NetworkService)
         }
         Spacer(minLength: 0)
     }
